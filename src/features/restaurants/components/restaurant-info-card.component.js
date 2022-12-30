95eb00067b5754da7a674834e04db24c2f761a19
NimrahAltafAdam <nimrahadam.123@gmail.com>
2022-12-30 19:29:17 +0500

    Optimzed Spacer component - Android

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -81,9 +81,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               </Text>
             )}
             {/*A Consistent SPACER FOR MYSELF*/ }
-            <Spacer variant="left.medium" />
+            <Spacer position="left" size="large" />
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant="left.medium" />
+            <Spacer position="left" size="large" />
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
--- src/features/restaurants/components/spacer/spacer.component.js
+++ src/features/restaurants/components/spacer/spacer.component.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+/*import React from 'react';
 import styled from 'styled-components/native';
 
 const TopSmall = styled.View`
@@ -43,4 +43,43 @@ export const Spacer = ({ variant }) => {
     return <LeftLarge />;
   }
   return <TopSmall />;
+};*/
+
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
+
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const positionVariant = {
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+
+  return `${property}:${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
+`;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
 };
